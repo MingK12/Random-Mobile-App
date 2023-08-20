ae79313d7c8fdaeaa93dc20e67a745067b168911
DESKTOP-JA0L6M0\gonca <goncalodomingos9@gmail.com>
2023-08-20 20:11:31 +0100

    toque inicial

---
+++ RanndomApp/RandomApp/Page/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
