caf3ca8356c93d099de59885f9624841772132d6
DESKTOP-JA0L6M0\gonca <goncalodomingos9@gmail.com>
2023-08-22 22:46:05 +0100

    toques

--- RanndomApp/RandomApp/Page/App.xaml.cs
+++ RanndomApp/RandomApp/Page/App.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using AudioUnit;
-
+using Microsoft.Maui.Controls;
 namespace RandomApp
 {
     public partial class App : Application
     {
+        public INavigation CurrentNavigation => ((MainPage)Current.MainPage).Navigation;
+        public Microsoft.Maui.Controls.Page CurrentPage => ((MainPage)Current.MainPage).Navigation.NavigationStack[((MainPage)Current.MainPage).Navigation.NavigationStack.Count - 1];
 
         public App()
         {
